d1525d81d6d8ef0f32ea5390a8f3a8eea67d6bc9
Vijay Khade <61784658+vjykhade@users.noreply.github.com>
2023-02-22 19:08:20 +0530

    Add files via upload

--- currency-conversion-service/src/main/java/com/work22022023/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/work22022023/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.work22022023.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
--- currency-conversion-service/src/main/java/com/work22022023/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/work22022023/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.work22022023.microservices.currencyconversionservice.controller;
 
 import com.work22022023.microservices.currencyconversionservice.model.CurrencyConversion;
+import com.work22022023.microservices.currencyconversionservice.proxy132.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,8 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)
     {
@@ -22,5 +26,11 @@ public class CurrencyConversionController {
         ResponseEntity<CurrencyConversion> responseEntity = new  RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class, uriVariables);
         CurrencyConversion conversion = responseEntity.getBody();
         return new CurrencyConversion(conversion.getId(),from,to,quantity,conversion.getConversionMultiple(),quantity.multiply(conversion.getConversionMultiple()),conversion.getEnvironment());
+    }
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)
+    {
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from,to);
+        return new CurrencyConversion(currencyConversion.getId(),from,to,quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
     }
 }
